70d49ebe8d61f050e5ade0597508ea90a4d56f87
Ronald Dias <ronaldsilva850@gmail.com>
2026-03-20 09:36:49 -0300

    feat: adicionados os enums PaymentMethod e PaymentStatus

---
+++ src/main/java/com/fadesp/payment_api/model/enums/PaymentMethod.java
@@ -0,0 +1,8 @@
+package com.fadesp.payment_api.model.enums;
+
+public enum PaymentMethod {
+    BOLETO,
+    PIX,
+    CARTAO_CREDITO,
+    CARTAO_DEBITO
+}
---
+++ src/main/java/com/fadesp/payment_api/model/enums/PaymentStatus.java
@@ -0,0 +1,7 @@
+package com.fadesp.payment_api.model.enums;
+
+public enum PaymentStatus {
+    PENDENTE,
+    PROCESSADO_SUCESSO,
+    PROCESSADO_FALHA
+}
